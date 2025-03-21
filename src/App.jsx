2c10e01d630389138a89eb908f6e59db3deddbdc
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-21 15:28:31 +0600

    coffee card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useLoaderData } from 'react-router-dom'
+import CoffeeCard from './Components/CoffeeCard/CoffeeCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const coffees = useLoaderData();
 
   return (
-    <>
-      <h1 className='text-6xl'>Coffee</h1>
-    </>
+    <div className='m-20'>
+      <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          coffees.map(coffee => <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+          // coffees={coffees}
+          // setCoffees={setCoffees}
+          ></CoffeeCard>)
+        }
+      </div>
+    </div>
   )
 }
 
--- src/Components/AddCoffee/AddCoffee.jsx
+++ src/Components/AddCoffee/AddCoffee.jsx
@@ -1,19 +1,47 @@
+import React from 'react';
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
 
-    const handleAddCoffee = (e) => {
+    const handleAddCoffee = e => {
         e.preventDefault();
-        const name = e.target.name.value;
-        const chef = e.target.chef.value;
-        const supplier = e.target.supplier.value;
-        const taste = e.target.taste.value;
-        const category = e.target.category.value;
-        const details = e.target.details.value;
-        const photo = e.target.photo.value;
+        const form = e.target;
 
-        const newCoffee = { name, chef, supplier, taste, category, details, photo }
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = { name, quantity, supplier, taste, category, details, photo }
         console.log(newCoffee)
+
+        // send data to the server and database
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    console.log('successfully added');
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee added successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Ok'
+                    });
+                    e.target.reset();
+                }
+            })
+
     }
+
     return (
         <div className='lg:w-3/4 mx-auto'>
             <div className="text-center p-10">
@@ -27,60 +55,45 @@ const AddCoffee = () => {
                 <form onSubmit={handleAddCoffee} className="card-body">
                     {/* form first row */}
                     <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input type="text" name='name' placeholder="coffee name" className="input input-bordered" required />
+                        <div className='form-control flex-1'>
+                            <label className="fieldset-label">Name</label>
+                            <input type="text" className="input" name="name" placeholder="Coffee Name" />
                         </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text">Chef</span>
-                            </label>
-                            <input type="text" name='chef' placeholder="chef name" className="input input-bordered" required />
+                        <div className='form-control flex-1'>
+                            <label className="fieldset-label">Available  Quantity</label>
+                            <input type="number" className="input" name="quantity" placeholder="Quantity" />
                         </div>
                     </div>
                     {/* form second row */}
                     <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text">Supplier</span>
-                            </label>
-                            <input type="text" name='supplier' placeholder="coffee supplier" className="input input-bordered" required />
+                        <div className='form-control flex-1'>
+                            <label className="fieldset-label">Supplier</label>
+                            <input type="text" name="supplier" className="input" placeholder="Supplier" />
                         </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text">Taste</span>
-                            </label>
-                            <input type="text" name='taste' placeholder="taste name" className="input input-bordered" required />
+                        <div className='form-control flex-1'>
+                            <label className="fieldset-label">Taste</label>
+                            <input type="text" className="input" name="taste" placeholder="Taste Name" />
                         </div>
                     </div>
-                    {/* form third row */}
+                    {/* form first row */}
                     <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text">Category</span>
-                            </label>
-                            <input type="text" name='category' placeholder="coffee Category" className="input input-bordered" required />
+                        <div className='form-control flex-1'>
+                            <label className="fieldset-label">Category</label>
+                            <input type="text" className="input" name="category" placeholder="Coffee Category" />
                         </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text">Details</span>
-                            </label>
-                            <input type="text" name='details' placeholder="Coffee Details" className="input input-bordered" required />
+                        <div className='form-control flex-1'>
+                            <label className="fieldset-label">Details</label>
+                            <input type="text" className="input" name="details" placeholder="Details" />
                         </div>
                     </div>
 
-
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Photo URL</span>
-                        </label>
-                        <input type="text" name='photo' placeholder="Photo url" className="input input-bordered" required />
-
+                    <div className='form-control'>
+                        <label className="fieldset-label">Photo Url</label>
+                        <input type="text" className="input w-full" name="photo" placeholder="Photo Url" />
                     </div>
+
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Add Coffee</button>
+                        <button className="btn btn-primary w-full">Add Coffee</button>
                     </div>
                 </form>
             </div>
